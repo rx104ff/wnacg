076bac690dd09ddf6545783ef6474ea30c325d5f
Ningyuan Gao <ning@152-84.priv23.nus.edu.sg>
2017-09-23 15:45:35 +0800

    Add the project
     with '#' will be ignored, and an empty message aborts the commit.

---
+++ src/main/kotlin/com/wnacg/wnacg/WnacgApplication.kt
@@ -0,0 +1,11 @@
+package com.wnacg.wnacg
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WnacgApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(WnacgApplication::class.java, *args)
+}
